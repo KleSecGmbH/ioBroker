023ee98196ed34ab7447b21e918252ffece7f6f0
CrunkFX <mavraj@klesec.de>
2021-11-07 15:55:32 +0100

    Create installer-pve.sh

---
+++ wireguard/installer-pve.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Updaten
+echo Update wird ausgeführt!
+
+apt update
+apt upgrade -y
+
+# Pakete laden
+echo Die erforderlichen Pakete werden geladen!
+
+apt install docker.io -y
+apt install docker-compose -y
+
+# Wireguard UI Konfigurieren
+echo Wireguard UI wird installiert!
+
+[ -f /root/wireguard-ui/docker-compose.yml ] || wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/docker-compose.yml -P /root/wireguard-ui
+[ -f /etc/systemd/system/wgui.path ] || wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.path -P /etc/systemd/system/
+[ -f /etc/systemd/system/wgui.service ] || wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.service -P /etc/systemd/system/
+
+wget git.io/wireguard -O wireguard-install.sh && bash wireguard-install.sh
