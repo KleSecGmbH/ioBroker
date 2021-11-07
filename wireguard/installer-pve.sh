64189af371ec4a0e06a21804389da2d86063b364
CrunkFX <mavraj@klesec.de>
2021-11-07 16:48:10 +0100

    Update installer-pve.sh

--- wireguard/installer-pve.sh
+++ wireguard/installer-pve.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Updaten
-echo Update wird ausgeführt!
+echo -e "Update \e[32mGreen"
 
 apt update
 apt upgrade -y
@@ -19,5 +19,5 @@ echo Wireguard UI wird installiert!
 [ -f /etc/systemd/system/wgui.path ] || wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.path -P /etc/systemd/system/
 [ -f /etc/systemd/system/wgui.service ] || wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.service -P /etc/systemd/system/
 
-wget git.io/wireguard -O wireguard-install.sh && bash wireguard-install.sh
-echo hallo
+wget git.io/wireguard -O wireguard-install.sh 
+bash wireguard-install.sh
