92b35cca5526dec4db51b4d9fbbcd08a003f8315
CrunkFX <mavraj@klesec.de>
2021-11-08 09:57:01 +0100

    Update installer-pve.sh

--- wireguard/installer-pve.sh
+++ wireguard/installer-pve.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+function print_centered {
+     [[ $# == 0 ]] && return 1
+
+     declare -i TERM_COLS="$(tput cols)"
+     declare -i str_len="${#1}"
+     [[ $str_len -ge $TERM_COLS ]] && {
+          echo "$1";
+          return 0;
+     }
+
+     declare -i filler_len="$(( (TERM_COLS - str_len) / 2 ))"
+     [[ $# -ge 2 ]] && ch="${2:0:1}" || ch=" "
+     filler=""
+     for (( i = 0; i < filler_len; i++ )); do
+          filler="${filler}${ch}"
+     done
+
+     printf "%s%s%s" "$filler" "$1" "$filler"
+     [[ $(( (TERM_COLS - str_len) % 2 )) -ne 0 ]] && printf "%s" "${ch}"
+     printf "\n"
+
+     return 0
+}
+
 ##========================== Variablen =============================##
 
 BLANK='\033[0m'
@@ -14,6 +38,7 @@ BOLD='\e[1m'
 
 ##====================== Terminal leermachen ========================##
 printf "\033c"
+print_centered "-" "-"
 
 ##======================= Farben definieren =========================##
 echo -e "wireguard-pve-install V1.0.0 Stand 08.11.2021                                                                             2021 forum.iobroker.net/user/crunkfx"
