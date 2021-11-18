1b015dee04bc842a79332de39cf1842f257f8092
CrunkFX <mavraj@klesec.de>
2021-11-18 17:04:20 +0100

    Update installer-wgui.sh

--- wireguard/installer-wgui.sh
+++ wireguard/installer-wgui.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+##################################
+########## Funktionen ############
+##################################
+
+
 ##=== Funktion zum zentrieren von Text von github.com/TrinityCoder ==##
 function print_centered {
      [[ $# == 0 ]] && return 1
@@ -25,31 +30,9 @@ function print_centered {
      return 0
 }
 
-##====================== Terminal leermachen ========================##
-printf "\033c"
+############# Update
 
-##============================= Header ==============================##
-print_centered "wireguard-ui-install V1.0.0 Stand 17.11.2021                                                                             2021 forum.iobroker.net/user/crunkfx" " "
-echo -e ""
-
-print_centered "-" "-"
-print_centered "                                                               " "#"
-print_centered "            Willkommen zum WireGuard UI-Installer            " "#"
-print_centered "                                                               " "#"
-print_centered "-" "-"
-##======================= Farben definieren =========================##
-echo -e ""
-echo -e ""
-echo -e ""
-print_centered "Dieser Installer wird Wireguard-UI, sowie alle notwendigen Pakete und Paketquellen laden und installieren." " "
-echo -e ""
-echo -e ""
-
-
-
-read -p "                            Wollen Sie fortfahren? (j/n)         " A
-if [ "$A" == "" -o "$A" == "j" ] || [ "$A" == "" -o "$A" == "y" ];then
-
+function update_system {
 # Updaten
 echo -e "\e[1;100m#### 1.   Updates werden geholt und Installiert\e[0m"
 
@@ -70,8 +53,16 @@ if [ $? -eq 0 ]; then
 else
    echo -e "\e[0;31m#Fehler\e[0m"
 fi
+} 
 
+read -p "                            Wollen Sie fortfahren? (j/n)         " A
+if [ "$A" == "" -o "$A" == "j" ] || [ "$A" == "" -o "$A" == "y" ];then
 
+############# Update Ende
+
+############# Pakete laden
+
+function getPackets {
 # Pakete laden
 echo -e "\e[1;100m#### 2.   Die erforderlichen Pakete werden geladen und installiert\e[0m"
 echo ""
@@ -90,9 +81,23 @@ if [ $? -eq 0 ]; then
 else
    echo -e "\e[0;31m#Fehler\e[0m"
 fi
+}
 
+############# Pakete laden ende
 
+############# WGUI entfernen
 
+function remove_wgui {
+docker kill wgui
+docker rm wgui
+rm -r /root/wireguard-ui
+}
+
+############# WGUI entfernen ende
+
+############# WGUI installieren
+
+function install_wgui {
 # Wireguard UI Konfigurieren
 echo -e "\e[1;100m#### 3.   WireGuard-UI wird installiert\e[0m"
 mkdir /root/wireguard-ui
@@ -100,13 +105,79 @@ wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/docker
 wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.path -O /etc/systemd/system/wgui.path
 wget https://raw.githubusercontent.com/KleSecGmbH/ioBroker/main/wireguard/wgui.service -O /etc/systemd/system/wgui.service
 
-
+echo -e "this is a new line \nthis is another new line" >> /root/wireguard-ui/docker-compose.yml
 
 cd /root/wireguard-ui
 docker-compose up -d
 
 systemctl enable wgui.{path,service}
 systemctl start wgui.{path,service}
+}
+
+############# WGUI installieren ende
+
+##################################
+########### Programm #############
+##################################
+
+##====================== Terminal leermachen ========================##
+printf "\033c"
+
+##============================= Header ==============================##
+print_centered "wireguard-ui-install V1.0.1 Stand 18.11.2021                                                                             2021 forum.iobroker.net/user/crunkfx" " "
+echo -e ""
+
+print_centered "-" "-"
+print_centered "                                                               " "#"
+print_centered "            Willkommen zum WireGuard UI-Installer            " "#"
+print_centered "                                                               " "#"
+print_centered "-" "-"
+##======================= Farben definieren =========================##
+echo -e ""
+echo -e ""
+echo -e ""
+print_centered "Dieser Installer wird Wireguard-UI, sowie alle notwendigen Pakete und Paketquellen laden und installieren." " "
+echo -e ""
+echo -e ""
+
+PS3='Was soll getan werden? : '
+options=("WireGuard UI installieren" "WireGuard UI neu-installieren" "WireGuard UI entfernen" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "OK WireGuard UI wird installiert"
+            update_system
+            getPackets
+            install_wgui
+            ;;
+        "Option 2")
+            echo "OK WireGuard UI wird neu-installiert"
+            update_system
+            getPackets
+            remove_wgui
+            install_wgui
+            ;;
+        "Option 3")
+            echo "OK WireGuard UI wird entfernt"
+            remove_wgui
+            ;;
+        "Quit")
+            exit
+            ;;
+        *) echo "Die Option $REPLY stand nicht zur auswahl :] ";;
+    esac
+done
+
+
+
+
+
+
+
+
+
+
 
 else
     echo -e "\e[1;41mInstallation abgebrochen!\e[0m"
