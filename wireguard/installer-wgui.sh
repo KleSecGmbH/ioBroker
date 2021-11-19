bf9c8f368e06b577fb9eb3563a64f601083ee1cc
CrunkFX <mavraj@klesec.de>
2021-11-19 20:46:58 +0100

    Update installer-wgui.sh

--- wireguard/installer-wgui.sh
+++ wireguard/installer-wgui.sh
@@ -13,30 +13,6 @@ DIR="/opt/wireguard-ui"
 #        Funktionen Start        #
 ##################################
 
-##=== Funktion zum zentrieren von Text von github.com/TrinityCoder ==##
-function print_centered {
-    [[ $# == 0 ]] && return 1
-    
-    declare -i TERM_COLS="$(tput cols)"
-    declare -i str_len="${#1}"
-    [[ $str_len -ge $TERM_COLS ]] && {
-        echo "$1"
-        return 0
-    }
-    
-    declare -i filler_len="$(((TERM_COLS - str_len) / 2))"
-    [[ $# -ge 2 ]] && ch="${2:0:1}" || ch=" "
-    filler=""
-    for ((i = 0; i < filler_len; i++)); do
-        filler="${filler}${ch}"
-    done
-    
-    printf "%s%s%s" "$filler" "$1" "$filler"
-    [[ $(((TERM_COLS - str_len) % 2)) -ne 0 ]] && printf "%s" "${ch}"
-    printf "\n"
-    
-    return 0
-}
 
 ############# Update
 function update_system {
